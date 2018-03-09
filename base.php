10a1d51c1c98662d7141051d8f63011d14565d68
odil-io <github@odil.io>
2018-03-09 13:13:07 +0100

    'Bouncer'-template toegevoegd
    De bedoeling is om tijdens alpha ontwikkeling een bouncer pagina te
    tonen. Wanneer er ingelogd is word de website in ontwikkeling getoont.

--- base.php
+++ base.php
@@ -3,37 +3,58 @@
 use Roots\Sage\Setup;
 use Roots\Sage\Wrapper;
 
+if( !is_user_logged_in() ):
+  get_template_part('bouncer');
+  exit;
+endif;
+
 ?>
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'imaga'); ?>
       </div>
     <![endif]-->
-    <?php
-      do_action('get_header');
-      get_template_part('templates/header');
-    ?>
+
+    <?php do_action('get_header'); ?>
+
+    <?php get_template_part('templates/header'); ?>
+
     <div class="wrap container" role="document">
+
       <div class="content row">
+
         <main class="main">
+
           <?php include Wrapper\template_path(); ?>
+
         </main><!-- /.main -->
+
         <?php if (Setup\display_sidebar()) : ?>
+
           <aside class="sidebar">
+
             <?php include Wrapper\sidebar_path(); ?>
+
           </aside><!-- /.sidebar -->
+
         <?php endif; ?>
+
       </div><!-- /.content -->
+
     </div><!-- /.wrap -->
-    <?php
-      do_action('get_footer');
-      get_template_part('templates/footer');
-      wp_footer();
-    ?>
+
+    <?php do_action('get_footer');?>
+
+    <?php get_template_part('templates/footer'); ?>
+
+    <?php wp_footer(); ?>
+
   </body>
+
 </html>
