6f55013e8360feee44ebda510d471625519bc626
odil-io <github@odil.io>
2018-07-26 15:35:08 +0200

    Fix: increased font size

--- templates/layouts/layout_facts.php
+++ templates/layouts/layout_facts.php
@@ -4,7 +4,7 @@
       <? if( have_rows('facts') ): ?>
         <? while ( have_rows('facts') ) : the_row(); ?>
           <div class="col">
-            <h2 class="display-4"><? the_sub_field('change'); ?><? the_sub_field('amount'); ?></h2>
+            <h2 class="display-2"><? the_sub_field('change'); ?><? the_sub_field('amount'); ?></h2>
             <p class="lead"><? the_sub_field('subject'); ?></p>
           </div>
         <? endwhile; ?>
