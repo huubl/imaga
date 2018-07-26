f5b96d5b9f2ba948fd50051710c42ecfbf8e8f32
odil-io <github@odil.io>
2018-07-26 15:50:38 +0200

    Feature: add Bootstrap Button shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -2,6 +2,16 @@
 
 namespace IMAGA\Theme\Shortcodes;
 
+/**
+ * Font Awesome Chevron Circle shortcode
+ */
 add_shortcode( 'chevron', function($atts){
   return '<i class="ml-2 fa fa-chevron-circle-' . $atts['type'] . '"></i>';
+});
+
+/**
+ * Bootstrap Button shortcode
+ */
+add_shortcode( 'button', function($atts){
+  return '<a class="btn btn-' . $atts['color'] . '" href="' . $atts['url'] . '">' . $atts['title'] . '</a>';
 });
