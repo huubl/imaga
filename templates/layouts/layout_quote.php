4f3fc10ec487b720d69bf6214b54a45f7f88800b
odil-io <github@odil.io>
2018-08-14 09:31:14 +0200

    Fix: added italic style to text

--- templates/layouts/layout_quote.php
+++ templates/layouts/layout_quote.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
       <div class="col-12 col-md-8">
         <blockquote class="blockquote text-center">
-          <?= Extras\heading_based_on_length( get_sub_field('quote') ); ?>
+          <i><?= Extras\heading_based_on_length( get_sub_field('quote') ); ?></i>
           <footer class="blockquote-footer <? the_sub_field('text_color'); ?>"><cite title="<? the_sub_field('author'); ?>"><? the_sub_field('author'); ?></cite></footer>
         </blockquote>
       </div>
