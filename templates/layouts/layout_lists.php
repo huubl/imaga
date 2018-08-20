7588f63413c4630bd391a44303530b2e0190f57e
odil-io <github@odil.io>
2018-08-20 15:51:55 +0200

    Add: layout lists

---
+++ templates/layouts/layout_lists.php
@@ -0,0 +1,20 @@
+<? use IMAGA\Theme\Extras; ?>
+
+<section class="layout-lists <? the_sub_field('background_color'); ?> <? the_sub_field('text_color'); ?>">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-md-center">
+        <?= Extras\heading_based_on_length( get_sub_field('title') ); ?>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-7">
+        <? if( have_rows('list') ): ?>
+          <? while ( have_rows('list') ) : the_row(); ?>
+            <h4 class="display-2"><i class="fa <? the_sub_field('icon'); ?>"></i> <? the_sub_field('title'); ?></h4>
+          <? endwhile; ?>
+        <? endif;?>
+      </div>
+    </div>
+  </div>
+</section>
