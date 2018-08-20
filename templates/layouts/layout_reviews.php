11b3843a7a395080f63dcb1354c37340627acec0
odil-io <github@odil.io>
2018-08-20 16:30:17 +0200

    Fix: add title field to review layout

--- templates/layouts/layout_reviews.php
+++ templates/layouts/layout_reviews.php
@@ -4,7 +4,7 @@
   <div class="container py-4">
     <div class="row">
       <div class="col-12 text-center py-4">
-        <h2 class="display-3">Don’t just take our word for it. Here’s what our great clients are saying:</h2>
+        <h2 class="display-3"><? the_sub_field('title');?></h2>
         <a class="lead text-muted link" href="#all-reviews">View all reviews <small><i class="fa fa-chevron-circle-right"></i></small></a>
       </div>
     </div>
