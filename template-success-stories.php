95bafba932d788bc83bbf8df4ef3861085fd8cde
odil-io <github@odil.io>
2018-08-23 16:15:44 +0200

    Refactoring

--- template-success-stories.php
+++ template-success-stories.php
@@ -11,14 +11,19 @@ use IMAGA\Theme\Extras;
   <div class="container">
 
     <div class="row justify-content-center text-center">
+
       <div class="col-12 col-md-8 pb-4 pb-md-0">
         <h1 class="display-1 mb-3"><? the_field('title'); ?></h1>
         <div class="lead mb-5"><? the_field('lead'); ?></div>
       </div>
+
       <a class="scroll-arrow bg-yellow" href="#content"><i class="fa fa-chevron-down"></i></a>
+
     </div>
+
   </div>
 </div>
+
 <div id="content"></div>
 
 <? while (have_posts()) : the_post(); ?>
