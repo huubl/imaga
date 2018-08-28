954fe938b26b942ca8fbe208f1d9f1504540ba61
odil-io <github@odil.io>
2018-08-28 12:13:42 +0200

    Update: chevron shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -3,10 +3,23 @@
 namespace IMAGA\Theme\Shortcodes;
 
 /**
- * Font Awesome Chevron Circle shortcode
+ * Font Awesome Chevron Circle shortcodes
  */
 add_shortcode( 'chevron', function($atts){
   return '<i class="ml-2 fa fa-chevron-circle-' . $atts['type'] . '"></i>';
+});
+add_shortcode( 'chevron-left', function($atts){
+  return '<i class="ml-2 fa fa-chevron-circle-left"></i>';
+});
+add_shortcode( 'chevron-right', function($atts){
+  return '<i class="ml-2 fa fa-chevron-circle-right"></i>';
+});
+
+/**
+ * Font Awesome Checkmark shortcode
+ */
+add_shortcode( 'checkmark', function($atts){
+  return '<i class="fa fa-check"></i>';
 });
 
 /**
