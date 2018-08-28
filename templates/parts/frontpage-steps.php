7298ff5cc27990e0d93913f18df879ec12042e9e
odil-io <github@odil.io>
2018-08-28 12:12:10 +0200

    Fix: frontpage to layout engine

---
+++ templates/parts/frontpage-steps.php
@@ -0,0 +1,34 @@
+
+<? use IMAGA\Theme\Assets; ?>
+
+<section class="bg-light">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-3">
+        <h2 class="lead mb-4">Or select any of the other steps of our conversion process</h2>
+      </div>
+      <div class="col-12 col-md-9">
+        <div class="row text-center">
+          <div class="col-6 col-md-4 mb-4 mb-lg-0">
+            <a href="/conversion-optimization/conversion-strategy">
+              <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/conversion-strategy-dark.svg');?>" alt="Conversion Strategy">
+              <h4 class="text-dark">Conversion<br>Strategy</h4>
+            </a>
+          </div>
+          <div class="col-6 col-md-4 mb-4 mb-lg-0">
+            <a href="/conversion-optimization/conversion-design">
+              <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/pencil-dark.svg');?>" alt="Conversion Design">
+              <h4 class="text-dark">Conversion<br>Design</h4>
+            </a>
+          </div>
+          <div class="col-12 col-md-4 mb-4 mb-lg-0">
+            <a href="/conversion-optimization/conversion-build">
+              <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/conversion-build-dark.svg');?>" alt="Conversion Build">
+              <h4 class="text-dark">Conversion<br>Build</h4>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
