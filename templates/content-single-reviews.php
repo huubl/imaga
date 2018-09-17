ec77eeabce4594e87dfd240f10d7e0870ae68139
odil-io <github@odil.io>
2018-09-17 17:24:25 +0200

    Beta: single-reviews template

--- templates/content-single-reviews.php
+++ templates/content-single-reviews.php
@@ -1,36 +1,13 @@
 
 <? use IMAGA\Theme\Extras; ?>
 
-<? get_template_part('templates/header','reviews'); ?>
 
 <? while (have_posts()) : the_post(); ?>
+
   <article <? post_class(); ?>>
-
-    <div class="row">
-      <div class="col-6">
-
-      </div>
-      <div class="col-6 box-shadow bg-white">
-        <div class="row">
-          <div class="col-3 text-right">
-            <img class="img-fluid img-circle" src="<?= get_the_post_thumbnail_url(); ?>" alt="<? the_title(); ?>">
-          </div>
-          <div class="col-9">
-            <? the_title(); ?>, <? the_field('company'); ?> <? the_field('location'); ?>
-            <span class="review-stars text-orange">
-              <? the_field('stars'); ?>
-            </span>
-            <p>
-              <small>
-                <? the_content(); ?>
-              </small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
+    <? get_template_part('templates/header','reviews'); ?>
   </article>
+  
 <? endwhile; ?>
 
 <? get_template_part('templates/parts/related-posts'); ?>
--- templates/header-reviews.php
+++ templates/header-reviews.php
@@ -1,9 +1,27 @@
-<section class="header-reviews bg-red text-white">
+<section class="header-reviews bg-red text-white py-md-7">
   <div class="container">
-    <div class="row">
-      <div class="col-12 col-md-6">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-5">
         <h1 class="display-1">Meet our customers.</h1>
         <p class="lead">See what small business owners have to say about how IMAGA improves their sales and reaches new customers.</p>
+      </div>
+      <div class="col-11 col-md-6 review">
+        <div class="row box-shadow bg-white text-body p-2">
+          <div class="col-3 text-right">
+            <img class="img-fluid img-circle" src="<?= get_the_post_thumbnail_url(); ?>" alt="<? the_title(); ?>">
+          </div>
+          <div class="col-9">
+            <? the_title(); ?>, <? the_field('company'); ?> <? the_field('location'); ?>
+            <span class="review-stars text-orange">
+              <? the_field('stars'); ?>
+            </span>
+            <p>
+              <small>
+                <? the_content(); ?>
+              </small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
