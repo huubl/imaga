6db5c62ee1b49a504264bd67bad62c135f985d02
odil-io <github@odil.io>
2018-09-18 17:18:08 +0200

    Fix: disable CRO Icons effect on summary template

--- templates/header-conversion-summary.php
+++ templates/header-conversion-summary.php
@@ -50,33 +50,25 @@ endif;
   <div class="container py-4">
     <div class="row text-center">
 
-      <div class="col-3">
-        <a href="/conversion-optimization/conversion-research#conversions">
-          <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-research-light.svg');?>" alt="Conversion Research">
-          <h4 class="d-none d-md-block text-white">Conversion<br>Research</h4>
-        </a>
-      </div>
+      <a class="col-3 conversion-category-item" href="/conversion-optimization/conversion-research#content">
+        <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-research-light.svg');?>" alt="Conversion Research">
+        <h4 class="d-none d-md-block text-white">Conversion<br>Research</h4>
+      </a>
 
-      <div class="col-3">
-        <a href="/conversion-optimization/conversion-strategy#conversions">
-          <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-strategy-light.svg');?>" alt="Conversion Strategy">
-          <h4 class="d-none d-md-block text-white">Conversion<br>Strategy</h4>
-        </a>
-      </div>
+      <a class="col-3 conversion-category-item" href="/conversion-optimization/conversion-strategy#content">
+        <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-strategy-light.svg');?>" alt="Conversion Strategy">
+        <h4 class="d-none d-md-block text-white">Conversion<br>Strategy</h4>
+      </a>
 
-      <div class="col-3">
-        <a href="/conversion-optimization/conversion-design#conversions">
-          <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-design-light.svg');?>" alt="Conversion Design">
-          <h4 class="d-none d-md-block text-white">Conversion<br>Design</h4>
-        </a>
-      </div>
+      <a class="col-3 conversion-category-item" href="/conversion-optimization/conversion-design#content">
+        <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-design-light.svg');?>" alt="Conversion Design">
+        <h4 class="d-none d-md-block text-white">Conversion<br>Design</h4>
+      </a>
 
-      <div class="col-3">
-        <a href="/conversion-optimization/conversion-build#conversions">
-          <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-build-light.svg');?>" alt="Conversion Build">
-          <h4 class="d-none d-md-block text-white">Conversion<br>Build</h4>
-        </a>
-      </div>
+      <a class="col-3 conversion-category-item" href="/conversion-optimization/conversion-build#content">
+        <img class="img-fluid mb-2" src="<?= Assets\asset_path('images/icons/conversion-build-light.svg');?>" alt="Conversion Build">
+        <h4 class="d-none d-md-block text-white">Conversion<br>Build</h4>
+      </a>
 
     </div>
   </div>
