cd07a74bfe3021b78348f1f9c45e0f013213e808
odil-io <github@odil.io>
2018-09-20 16:37:57 +0200

    Remove: 'View all reviews' link

--- templates/layouts/layout_reviews.php
+++ templates/layouts/layout_reviews.php
@@ -5,7 +5,6 @@
     <div class="row">
       <div class="col-12 text-center py-4">
         <h2 class="display-3"><? the_sub_field('title');?></h2>
-        <a class="lead text-muted link" href="#all-reviews">View all reviews <small><i class="fa fa-chevron-circle-right"></i></small></a>
       </div>
     </div>
   </div>
