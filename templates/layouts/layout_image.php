6f9a39ca39b22ec43cd20605e94e2cee8e01ea65
odil-io <github@odil.io>
2018-10-25 16:48:45 +0200

    Dev: added option to cover image

--- templates/layouts/layout_image.php
+++ templates/layouts/layout_image.php
@@ -5,14 +5,23 @@ $image = get_sub_field('image'); ?>
 
 <? if( !empty($image) ): ?>
 
-  <section id="section-<?= get_row_index(); ?>" class="layout-image <? the_sub_field('background_color'); ?>">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <img class="img-fluid" src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+  <? if( get_sub_field('appearence') == 'cover' ): ?>
+
+    <section id="section-<?= get_row_index(); ?>" class="layout-image bg-cover <? the_sub_field('background_color'); ?>" style="background-image: url(<?= $image['url']; ?>); min-height:<? the_sub_field('height'); ?>px">
+    </section>
+
+  <? elseif( get_sub_field('appearence') == 'contain' ): ?>
+
+    <section id="section-<?= get_row_index(); ?>" class="layout-image <? the_sub_field('background_color'); ?>">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <img class="img-fluid" src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>" />
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+  <? endif; ?>
 
 <? endif; ?>
