6a991a9322a8c09ffa9a045da6a7f4fe532b492e
odil-io <github@odil.io>
2018-10-31 16:42:06 +0100

    Feature: added Layout Brands

--- templates/layouts/layout_brands.php
+++ templates/layouts/layout_brands.php
@@ -4,14 +4,12 @@
   <div class="container">
     <div class="row">
       <div class="col-12 text-md-center">
-        <h4>You’re in good company. We're trusted by agencies and smart companies such as:</h4>
+        <h4><? the_sub_field('title'); ?></h4>
       </div>
     </div>
     <div class="row justify-content-center">
       <div class="col-12">
-
-        <? Extras\the_brands( get_field('brands') ); ?>
-
+        <? Extras\the_brands( get_sub_field('brands') ); ?>
       </div>
     </div>
   </div>
