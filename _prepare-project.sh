fdaabfa54cde2cfdec00ba0918af6e8daa316016
odil-io <github@odil.io>
2018-11-08 16:53:40 +0100

    Dev: slight 'improvements' to project prepare file

--- _prepare-project.sh
+++ _prepare-project.sh
@@ -4,16 +4,16 @@ echo "This might take a while...";
 
 tput bold; echo "Running NPM install."
 tput sgr0
-npm install || exit 1
+npm install &> log/npmlog.txt || exit 1
 
 tput bold; echo "Running composer install..."
 tput sgr0
-composer install || exit 1
+composer install &> log/composerlog.txt || exit 1
 
 tput bold; echo "Running bower install..."
 tput sgr0
-bower install || exit 1
+bower install > log/bowerlog.txt || exit 1
 
 tput bold; echo "Running gulp..."
 tput sgr0
-gulp || exit 1
+gulp &> log/gulplog.txt || exit 1
