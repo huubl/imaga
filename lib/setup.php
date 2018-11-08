61f1bbb741feed70c25b48f8ca30c9ad4af9d0b9
odil-io <github@odil.io>
2018-11-08 16:55:30 +0100

    Dev: add functions for Contact Form 7

--- lib/setup.php
+++ lib/setup.php
@@ -162,3 +162,14 @@ if( function_exists('acf_add_options_page') ) {
 	));
 
 }
+
+/**
+* Disable auto-paragraphing for Contact Form 7
+*/
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+
+    return $content;
+});
