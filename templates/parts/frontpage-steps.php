df2ce74943ee4ddc33d1523d34caf73c56d614d2
odil-io <github@odil.io>
2018-11-08 16:54:10 +0100

    Fix: removed step 4 (build)

--- templates/parts/frontpage-steps.php
+++ templates/parts/frontpage-steps.php
@@ -5,34 +5,34 @@
   <div class="container">
     <div class="row align-items-center">
       <div class="col-12 text-center">
-        <h2 class="lead mb-4">We help you see what's happing on your website</h2>
+        <h2 class="lead mb-4">Discover Which Conversion Service Is Right For Your Business</h2>
       </div>
     </div>
     <div class="row justify-content-center">
-      <div class="col-10">
+      <div class="col-12 col-md-10">
         <div class="row text-center">
-          <div class="col-6 col-md-3 mb-4 mb-lg-0">
+          <div class="col-12 col-md mb-4 mb-lg-0">
             <a href="/conversion-optimization/conversion-research">
               <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/conversion-research-dark.svg');?>" alt="Conversion Research">
-              <h4 class="text-dark">Conversion<br>Research</h4>
+              <h4 class="text-dark">
+                <div class="d-inline d-md-block">Conversion</div> <div class="d-inline d-md-block">Research</div>
+              </h4>
             </a>
           </div>
-          <div class="col-6 col-md-3 mb-4 mb-lg-0">
+          <div class="col-12 col-md mb-4 mb-lg-0">
             <a href="/conversion-optimization/conversion-strategy">
               <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/conversion-strategy-dark.svg');?>" alt="Conversion Strategy">
-              <h4 class="text-dark">Conversion<br>Strategy</h4>
+              <h4 class="text-dark">
+                <div class="d-inline d-md-block">Conversion</div> <div class="d-inline d-md-block">Strategy</div>
+              </h4>
             </a>
           </div>
-          <div class="col-6 col-md-3 mb-4 mb-lg-0">
+          <div class="col-12 col-md mb-4 mb-lg-0">
             <a href="/conversion-optimization/conversion-design">
               <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/pencil-dark.svg');?>" alt="Conversion Design">
-              <h4 class="text-dark">Conversion<br>Design</h4>
-            </a>
-          </div>
-          <div class="col-6 col-md-3 mb-4 mb-lg-0">
-            <a href="/conversion-optimization/conversion-build">
-              <img class="img-fluid mb-3" src="<?= Assets\asset_path('images/icons/conversion-build-dark.svg');?>" alt="Conversion Build">
-              <h4 class="text-dark">Conversion<br>Build</h4>
+              <h4 class="text-dark">
+                <div class="d-inline d-md-block">Conversion</div> <div class="d-inline d-md-block">Design</div>
+              </h4>
             </a>
           </div>
         </div>
