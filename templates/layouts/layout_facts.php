d8a222d9f27a15f2b4b9987e952c8eb68cb86635
odil-io <github@odil.io>
2018-11-13 17:28:41 +0100

    Fix: showing '1' when it should be empty

--- templates/layouts/layout_facts.php
+++ templates/layouts/layout_facts.php
@@ -4,7 +4,7 @@
       <? if( have_rows('facts') ): ?>
         <? while ( have_rows('facts') ) : the_row(); ?>
           <div class="col-12 col-md">
-            <h2 class="display-2"><?= ( get_sub_field('change') == 'none' ) ?: get_sub_field('change') ; ?><? the_sub_field('amount'); ?></h2>
+            <h2 class="display-2"><?= ( get_sub_field('change') == 'none' )?'':get_sub_field('change'); ?><? the_sub_field('amount'); ?></h2>
             <p class="lead"><?= do_shortcode( get_sub_field('subject') ); ?></p>
           </div>
         <? endwhile; ?>
