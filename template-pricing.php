a2c2d02c2d9b482fa18b7cef56a15cce763f5841
odil-io <github@odil.io>
2018-11-16 13:25:46 +0100

    Dev: new Pricing table and template

---
+++ template-pricing.php
@@ -0,0 +1,93 @@
+<?
+/**
+* Template Name: Prijslijst [hardcoded]
+*/
+
+use IMAGA\Theme\Extras;
+?>
+
+<? while (have_posts()) : the_post(); ?>
+
+  <? get_template_part('templates/header','pricing'); ?>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h1 class="display-1">Introduction Package</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+        </div>
+        <div class="col-6">
+          <h2><sup class="text-red"><small><s>€4994,-</s></small></sup> &euro; 1999,-</h2>
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+          <ul class="list-checkmark">
+            <li>Google Analytics Audit & Health Check <sup class="text-muted">€299,-</sup></li>
+            <li>Conversion Formula Session <sup class="text-muted">€799,-</sup></li>
+            <li><abbr title="Search Engine Optimization">SEO</abbr>-Audit with Google Search Insights <sup class="text-muted">€499,-</sup></li>
+            <li>Brandkey and Archetype Branding Strategy (2 Sessions) <sup class="text-muted">€1199,-</sup></li>
+            <li>Web Analytics Research (Google Analytics) <sup class="text-muted">€600,-</sup></li>
+            <li>Usability Expert Review <sup class="text-muted">€1099,-</sup></li>
+            <li>Technical Analysis and Speed Optimization <sup class="text-muted">€499,-</sup></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h2><sup class="text-red"><small><s>€4994,-</s></small></sup> &euro; 2999,-</h2>
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+          <ul class="list-checkmark">
+            <li>Google Analytics Audit & Health Check <sup class="text-muted">€299,-</sup></li>
+            <li>Conversion Formula Session <sup class="text-muted">€799,-</sup></li>
+            <li><abbr title="Search Engine Optimization">SEO</abbr>-Audit with Google Search Insights <sup class="text-muted">€499,-</sup></li>
+            <li>Brandkey and Archetype Branding Strategy (2 Sessions) <sup class="text-muted">€1199,-</sup></li>
+            <li>Web Analytics Research (Google Analytics) <sup class="text-muted">€600,-</sup></li>
+            <li>Usability Expert Review <sup class="text-muted">€1099,-</sup></li>
+            <li>Technical Analysis and Speed Optimization <sup class="text-muted">€499,-</sup></li>
+          </ul>
+        </div>
+        <div class="col-6">
+          <h1 class="display-1">I Want Comprehensive Insights</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h1 class="display-1">I Want It All</h1>
+          <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+        </div>
+        <div class="col-6">
+          <h2><sup class="text-red"><small><s>€4994,-</s></small></sup> &euro; 3999,-</h2>
+          <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+          <ul class="list-checkmark">
+            <li>Google Analytics Audit & Health Check <sup class="text-muted">€299,-</sup></li>
+            <li>Conversion Formula Session <sup class="text-muted">€799,-</sup></li>
+            <li><abbr title="Search Engine Optimization">SEO</abbr>-Audit with Google Search Insights <sup class="text-muted">€499,-</sup></li>
+            <li>Brandkey and Archetype Branding Strategy (2 Sessions) <sup class="text-muted">€1199,-</sup></li>
+            <li>Web Analytics Research (Google Analytics) <sup class="text-muted">€600,-</sup></li>
+            <li>Usability Expert Review <sup class="text-muted">€1099,-</sup></li>
+            <li>Technical Analysis and Speed Optimization <sup class="text-muted">€499,-</sup></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <? if( have_rows('layouts') ): ?>
+    <? while( have_rows('layouts') ): the_row(); ?>
+
+      <? Extras\get_layout( get_row_layout() ); ?>
+
+    <? endwhile; ?>
+  <? endif; ?>
+
+<? endwhile; ?>
