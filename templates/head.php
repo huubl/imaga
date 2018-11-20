9165764ef74f4ca677e07276f3d0cde86f9990e3
odil-io <github@odil.io>
2018-11-20 10:25:40 +0100

    Dev: add no-referrer meta

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="referrer" content="no-referrer">
   <? wp_head(); ?>
 </head>
